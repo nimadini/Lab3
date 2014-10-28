cdd33efb7b5062d2327ec0a6fe7e443fd18baf9c
nimadini <nima.dini@gmail.com>
2014-10-28 02:38:22 -0500

    some test cases removed for changing test coverage 3

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -3,9 +3,6 @@ import Experiment
 
 class MyTestCase(unittest.TestCase):
 
-  def test_t1(self):
-    r1 = Experiment.largest([1,1,1]) 
-    self.assertEqual(r1, 1)
 
 if __name__ == '__main__':
   unittest.main() 
